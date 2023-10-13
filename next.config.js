b5249c8213f7d1647279c74e80cb094d5cb77ca8
Matheus Cruz <matheuscruz.dev@gmail.com>
2023-10-13 13:42:46 -0300

    ci: change Dockerfile

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'standalone',
+    env: {
+        PLATFORMOON_APPS_BASE_URL: process.env.PLATFORMOON_APPS_BASE_URL || "http://0.0.0.0:8080"
+    }
+}
 
 module.exports = nextConfig
--- src/conf/axios/index.ts
+++ src/conf/axios/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://0.0.0.0:8080",
+  baseURL: process.env.PLATFORMOON_APPS_BASE_URL,
 });
