6cbd2e1cb5478199061a6fd7df19e9defd25f802
Matheus Cruz <matheuscruz.dev@gmail.com>
2023-10-14 20:43:08 -0300

    chore: remove default base url

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        PLATFORMOON_APPS_BASE_URL: process.env.PLATFORMOON_APPS_BASE_URL || "http://0.0.0.0:8080"
+        PLATFORMOON_APPS_BASE_URL: process.env.PLATFORMOON_APPS_BASE_URL
     },
 }
 
